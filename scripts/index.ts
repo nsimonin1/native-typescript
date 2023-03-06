47a7466613b81457e84ecd5f1c941931d1344355
nsimonin1 <nsimonin1@gmail.com>
2023-03-06 06:41:43 +0100

    Init typescript

--- scripts/index.ts
+++
@@ -1,7 +0,0 @@
-
-const tag = document.createElement("b");
-tag.textContent = 'hellos';
-const doc = document.getElementById('main');
-
-
-doc.appendChild(tag);
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+        {
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_module/,
+        },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+    }),
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  /*optimization: {
+    runtimeChunk: 'single',
+  },*/
+};
